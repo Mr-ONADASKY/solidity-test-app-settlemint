0b162a5ae93cc65a73a32963cb7a98ea816e20f7
ninjawulf98 <ninjawulf98@gmail.com>
2018-11-11 21:25:54 +0100

    test

--- truffle.js
+++ truffle.js
@@ -1,5 +1,8 @@
 // Allows us to use ES6 in our migrations and tests.
-require('babel-register')
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var infura_apikey = "1f842038843c4210b7705157c23fbee8";
+var mnemonic = "some awesome mneumonic";
 
 module.exports = {
   networks: {
@@ -8,6 +11,12 @@ module.exports = {
       port: 8545,
       network_id: '*',
       gas: 6600000
+    },
+    ropsten: {
+        provider: new HDWalletProvider(mnemonic, 
+                  "https://ropsten.infura.io/v3/" + infura_apikey),
+        network_id: 3,
+        gas: 400000
     }
   }
 }
