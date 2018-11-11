e1e70c04d87520a99c5981dae771ab4698b5c5ff
Nick <ninjawulf98@gmail.com>
2018-11-11 12:34:28 +0100

    Added some functions

---
+++ app/javascripts/app.js
@@ -0,0 +1,74 @@
+// Import the page's CSS. Webpack will know what to do with it.
+import "../stylesheets/app.css";
+
+// Import libraries we need.
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract'
+
+/*
+ * When you compile and deploy your Voting contract,
+ * truffle stores the abi and deployed address in a json
+ * file in the build directory. We will use this information
+ * to setup a Voting abstraction. We will use this abstraction
+ * later to create an instance of the Voting contract.
+ * Compare this against the index.js from our previous tutorial to see the difference
+ * https://gist.github.com/maheshmurthy/f6e96d6b3fff4cd4fa7f892de8a1a1b4#file-index-js
+ */
+
+import voting_artifacts from '../../build/contracts/Voting.json'
+
+var Voting = contract(voting_artifacts);
+
+var candidates = [];
+
+window.voteForCandidate = (candidateId) => {
+  try {
+    $("#msg").html("Vote has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.")
+    $("#candidate").val("");
+
+    /* Voting.deployed() returns an instance of the contract. Every call
+     * in Truffle returns a promise which is why we have used then()
+     * everywhere we have a transaction call
+     */
+    Voting.deployed().then((contractInstance) => {
+      contractInstance.voteForCandidate(candidateId, {gas: 140000, from: web3.eth.accounts[0]});
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+$( document ).ready(() => {
+  if (typeof web3 !== 'undefined') {
+    console.warn("Using web3 detected from external source like Metamask")
+    // Use Mist/MetaMask's provider
+    window.web3 = new Web3(web3.currentProvider);
+  } else {
+    window.alert("No metamask detected, please install metamask to use this DApp");
+  }
+
+  Voting.setProvider(web3.currentProvider);
+  //get the amount of candidates from within the contract and add them to the html table
+  Voting.deployed().then((contractInstance) => {
+  contractInstance.votedEvent().watch((error, result) => {
+      console.log(error, event);
+  })
+
+   contractInstance.candidatesCount.call().then((amountOfCandidates) => {
+     console.log(amountOfCandidates.toString());
+    for (var i = 0; i < amountOfCandidates; i++) {
+        contractInstance.candidates(i).then((candidate) => {
+          let candidateId = candidate[0]; //the id from the candidate
+          let candidateName = candidate[1].toString(); //the name from the candidate
+          let candidateVoteCount = candidate[2]; // the amount of votes for the candidate
+          var htmlString = "<tr><td>" + candidateName + "</td><td id='candidate-" 
+                                + candidateId + "'>" + candidateVoteCount + "</td><td>"
+                                + "<a href='#' onclick='voteForCandidate(" + candidateId 
+                                + ")' class='btn btn-primary'>Vote</a></td></tr>";
+          $("#table-candidate").append(htmlString);
+          candidates[i] = candidateName; //store the candidate names in an array for easy access
+        });
+    }
+   });
+  });  
+});
---
+++ contracts/Voting.sol
@@ -0,0 +1,127 @@
+/*sources: dappuniversity https://github.com/dappuniversity/election
+            https://ethereum.stackexchange.com/questions/2519/how-to-convert-a-bytes32-to-string
+            https://medium.com/@mvmurthy/full-stack-hello-world-voting-ethereum-dapp-tutorial-part-1-40d2d0d807c2
+            https://medium.com/@mvmurthy/full-stack-hello-world-voting-ethereum-dapp-tutorial-part-2-30b3d335aa1f
+*/
+pragma solidity ^0.4.24; //We have to specify what version of compiler this code will use
+
+import './Owned.sol';
+
+contract Voting is Owned{
+
+      //Model for candidate
+      struct Candidate {
+        uint id;        //the id from the candidate
+        string name;     //the namefrom the candidate
+        uint voteCount;   //the amount of votes for each candidate
+      }
+
+      //Store accounts that have voted 
+      mapping(address => bool) public voters;
+
+      //Store Candidates
+      mapping(uint => Candidate) public candidates;
+
+      //Store the amount of candidates
+      uint public candidatesCount;
+
+      // voted event
+      event votedEvent (uint indexed _candidateId);
+
+      // vote erased
+      event voteRemovedEvent (uint indexed _candidateId);
+
+      // vote changed
+      event voteChanged (uint indexed _oldCandidateId, uint indexed _candidateId);
+
+      // error event
+      event revert (string error);
+
+      // constructor for the voting contract
+      function Voting (bytes32[] candidateNames) public {
+        uint candidateNamesLength = candidateNames.length;
+           for(uint index = 0; index < candidateNamesLength; index++){    
+            string memory candidateNameString =  convertBytes32ToString(candidateNames[index]); //since there's no native support we need to convert this to strings
+            addCandidate(candidateNameString);
+          }
+      }
+
+    // convert the bytes32 to strings
+    function convertBytes32ToString(bytes32 bytes32String) private returns(string){
+      bytes memory bytesArray = new bytes(32);
+      for (uint256 i; i < 32; i++) {
+        bytesArray[i] = bytes32String[i];
+      }
+      return string(bytesArray);
+    }
+
+    // add a candidate
+    function addCandidateDuringPoll (string _name) onlyOwner public{
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        candidatesCount ++;
+    }
+
+    
+    // add a candidate
+    function addCandidate (string _name) private{
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+        candidatesCount ++;
+    }
+
+    // vote for the candidate
+    function voteForCandidate (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender], "You have already voted!");
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        votedEvent(_candidateId);
+    } 
+
+    // remove the vote
+    function removeVote(uint _candidateId) public {
+      //require that they have already voted
+      require(voters[msg.sender], "You haven't voted yet!");
+
+      // require a valid candidate
+      require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+      //reduce the vote count from the candidate
+      candidates[_candidateId].voteCount--;
+
+      // delete the votecount from the sender to allow to revote
+      delete voters[msg.sender];
+
+      //trigger the vote removed event
+      voteRemovedEvent(_candidateId);
+    }
+
+    // change vote
+    function changeVote(uint _oldCandidateId, uint _candidateId) {
+      //require that they have already voted
+      require(voters[msg.sender], "You haven't voted yet!");
+
+       // require a valid candidate to remove vote from
+      require(_oldCandidateId > 0 && _oldCandidateId <= candidatesCount);
+
+       // require a valid candidate to vote for
+      require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+      // reduce the votes from the previous candidate on which the user voted on
+      candidates[_oldCandidateId].voteCount--;
+
+      // increase the votes from the new candidate on which the user wants to vote
+      candidates[_candidateId].voteCount++;
+    }
+}
+
+
+
